7377ff5e4a3c56ddf059c89447b8aa126a8b4f11
Alvin Voo <alvin.voo@gmail.com>
2018-06-28 12:54:19 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# ReduxSimpleStarter
+# Simple Youtube Interface with React
+
+![youtubereact](https://github.com/Alvin-Voo/youtubereact/blob/master/Screenshot1.png "youtubereact")
+
+Learn more from Stephen Grider with below link:
 
 Interested in learning [Redux](https://www.udemy.com/react-redux/)?
 
 ### Getting Started
 
-There are two methods for getting started with this repo.
-
-#### Familiar with Git?
-Checkout this repo, install dependencies, then start the gulp process with the following:
+This project is built with Stephen's starter kit:
 
 ```
 > git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';//work with components
+import ReactDOM from 'react-dom';//work with rendering components INTO DOM
+import YTSearch from 'youtube-api-v3-search';
+import _ from 'lodash';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyC4hQaaxHAVmE9ar0anpEhmvMql49rWOTs';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const options = {
+  q:'cats',
+  part:'snippet',
+  // pageToken:'CAUQAA',
+  // maxResults:25,
+  type:'video'
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//create a new component with JSX
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      videos:[],
+      selectedVideo:null
+    };
+
+    this.videoSearch('cats');
+
+  }
+
+  videoSearch(q){
+    YTSearch(API_KEY,{q, part:'snippet', type:'video'}).then(
+      data=>{
+        this.setState({
+          videos: data.items,
+          selectedVideo: data.items[0]
+        });
+      }
+    )
+  }
+
+  render(){
+    const videoSearch = _.debounce((term)=>{this.videoSearch(term)},300);
+    return(
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+        videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+
+//Take this component and put it into the DOM
+ReactDOM.render(<App/>,document.querySelector('.container'));//Need to instantiate a component instead of passing a component class
+//App is just returning a JSX template
+//Wrapping with JSX tag will create a component instance with --> React.createElement("...."),
